1e17e2cfc8b29e1c8b2701354377fc76f3d0e025
cnicholas <chris.nicholas@gmail.com>
2022-10-15 16:34:39 -0500

    refactored function for setting choices in select input for x y and t

--- R/app.R
+++ R/app.R
@@ -50,28 +50,30 @@ vasApp <- function(...) {
       message("in input_file ObserveEvent")
       req(dataset())
       message(paste("Column names are: ",names(dataset())))
-      response_choices <- names(dplyr::select_if(dataset(), is.numeric))
+      response_choices <- get_choices(names(dplyr::select_if(dataset(), is.numeric)))
       updateSelectInput(session, "variable_response", choices = response_choices, selected='') #set selected to '' to avoid triggering observe
     })
 #GET Analysis structure defined
     observeEvent(input$variable_response, {
       if(input$variable_response !='')
         message(paste("Observing variable_response changes: ", input$variable_response))
-        unselected_variables<- !stringr::str_detect(names(dataset()),input$variable_response)
-        message(unselected_variables)
-        time_variable_choices<-names(dataset()[unselected_variables])
+
+        time_variable_choices<-get_choices(names(dataset()),input$variable_response)
         message(time_variable_choices)
         updateSelectInput(session, "variable_time", choices = time_variable_choices, selected='')
         updateSelectInput(session, "variable_rsg", choices = time_variable_choices, selected='')
     })
+    #set choices parameters (control, selected_variables<character vector) returns vector of choices
+    get_choices<-function(all_choices, selected=c("")){
+      all_choices[!all_choices%in%selected]
+    }
+
     observeEvent(input$variable_time, {
       if(input$variable_time !='')
         message(paste("Observing variable_time changes: ", input$variable_time))
       #filter remaining variable by y and t to populate options for rsg variables
-      unselected_variables<- !names(dataset())%in%c(input$variable_response, input$variable_time)
-      message(unselected_variables)
 
-      rsg_variable_choices<-names(dataset()[unselected_variables])
+      rsg_variable_choices<-get_choices(names(dataset()),c(input$variable_response,input$variable_time))
       message(rsg_variable_choices)
       updateSelectInput(session, "variable_rsg", choices = rsg_variable_choices, selected='')
     })
