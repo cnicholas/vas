089ea10244af413ceb91ba389dd1c61bea844931
cnicholas <chris.nicholas@gmail.com>
2022-10-17 15:47:18 -0500

    Added additional capabilities for full and summary rsg data and enhanced sorting and display for analytic data set.

--- R/app.R
+++ R/app.R
@@ -18,13 +18,21 @@ vasApp <- function(...) {
         selectInput("variable_time",label = "Time Dimension (t):", choices=NULL),
         selectInput("variable_rsg",label = "RSG Variables (x):", choices=NULL, multiple = TRUE),
         actionButton("analyze",label = "Analyze"),
-        tableOutput("rsg_stats")
+        hr(),
+        tableOutput("rsg_stats"),
+        textOutput("disclaimer")
 
       ),
       mainPanel(
         tabsetPanel(id="tabset",
-                    tabPanel(value="tab_data",title = "Data",
-                             tableOutput("dataSummary")),
+                    tabPanel(value="tab_data",title = "Original Data",
+                             dataTableOutput("dataSummary")
+                    ),
+                    tabPanel(value="tab_rsg_data", title="Analysis Dataset",
+                            "The Analysis dataset excludes missing values and is sorted by rational subgroup and time",
+                            hr(),
+                            dataTableOutput("rsg_full")
+                    ),
                     tabPanel(value="tab_data_stats", title="Summary Statistics"),
                     tabPanel(value="tab_charts",title="Charts",
                              textOutput("greeting"),
@@ -51,6 +59,7 @@ vasApp <- function(...) {
 
     })
     output$datafilestats<-renderTable(dataset()%>%summarize(Observations=n(), Variables=ncol(.), "Missing Values"=sum(is.na(.))))
+
     observeEvent(input$datafile, {
       message("in input_file ObserveEvent")
       req(dataset())
@@ -85,36 +94,50 @@ vasApp <- function(...) {
     observeEvent(input$variable_rsg,{
       message(paste("RSG defined: ", input$variable_rsg))
     })
-    observeEvent(input$analyze, {
-      y<- input$variable_response
-      t<- input$variable_time
-      rsg<- input$variable_rsg
-      req(y,t,rsg)
 
-      message(rsg)
-    })
 
-    rsgs<- reactive({
+    rsgs_info<- reactive({
       if(input$analyze==0)
         return(NULL)
-      req(dataset())
+
+      req(dataset(),input$variable_rsg,input$variable_response,input$variable_time)
       message(paste("in actionbutton: ",input$analyze))
+
       #need to create list of symbols to do dynamic calculations
       rsg_def <- lapply(input$variable_rsg, as.symbol)
 
       rsg_col_name <-paste0(input$variable_rsg,collapse="_")
       message(paste0("rsg col name: ",rsg_col_name))
       #fill_weights%>%mutate(!!var:=paste0(!!!cols))%>%head()
-      dataset() %>% mutate(!!rsg_col_name := paste(!!!rsg_def, sep="_")) %>%
-                             #group_by_at(.vars=input$variable_rsg) %>%
-                             group_by(!!sym(rsg_col_name)) %>%
-        summarize("Mean (y)" = mean(!!sym(input$variable_response), na.rm=TRUE),
+
+      #Return a list of data frames 1 for summary and 1 with added columns
+      #Build Rational Subgroup Fullset with RSG column
+      full <-dataset() %>%  mutate(!!rsg_col_name := paste(!!!rsg_def, sep="_")) %>%
+        filter(is.na(!!sym(input$variable_response))==FALSE) %>%
+        arrange(!!sym(rsg_col_name), !!sym(input$variable_time)) %>%
+        select(!!sym(rsg_col_name),!!sym(input$variable_time),!!sym(input$variable_response), everything())
+
+      message(nrow(full))
+      #Build Rational Subgroup Summary
+      summary<-dataset() %>%
+                mutate(!!rsg_col_name := paste(!!!rsg_def, sep="_")) %>%
+                group_by(!!sym(rsg_col_name)) %>%
+                summarize(n=n(),
+                  "Mean (y)" = mean(!!sym(input$variable_response), na.rm=TRUE),
                   "sd (y)" = sd(!!sym(input$variable_response), na.rm=TRUE),
                   "Missing Values"=sum(is.na(!!sym(input$variable_response))))
-    })
-    output$rsg_stats<-renderTable(rsgs())
-    output$dataSummary<-renderTable(dataset())
 
+      list(summary=summary, full=full)
+    })
+    disclaimer_msg<-reactive({
+      req(rsgs_info())
+      if(sum(rsgs_info()$summary$"Missing Values")>0)
+        "Note: Missing values removed for calculation of the mean and standard deviation!"
+    })
+    output$rsg_stats<-renderTable(rsgs_info()$summary)
+    output$dataSummary<-renderDataTable(dataset())
+    output$disclaimer<-renderText(disclaimer_msg())
+    output$rsg_full<- renderDataTable(rsgs_info()$full)
   }
   shinyApp(ui, server, ...)
 }
